b0d7b12dbf0cb6710de564f5156ef8c7af0f7d9b
Renan Souza <renan-plsouza@uol.com.br>
2022-02-13 12:12:25 -0300

    Adição do método para chamada de operação de remoção

--- src/main/java/com/pingr/Connections/application/KafkaConsumerService.java
+++ src/main/java/com/pingr/Connections/application/KafkaConsumerService.java
@@ -2,6 +2,7 @@ package com.pingr.Connections.application;
 
 import com.pingr.Connections.core.Account;
 import com.pingr.Connections.core.events.AccountCreatedEvent;
+import com.pingr.Connections.core.events.AccountRemovedEvent;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +15,16 @@ public class KafkaConsumerService {
             groupId = "${spring.kafka.consumer.group-id}"
     )
     public void handleAccountCreation(AccountCreatedEvent event) {
+        Account account = event.extract();
+        System.out.println(account);
+    }
+
+    @KafkaListener(
+            containerFactory = "accountRemovedEventKafkaListenerContainerFactory",
+            topics = "${topics.acc_removed}",
+            groupId = "${spring.kafka.consumer.group-id}"
+    )
+    public void handleAccountDeletion(AccountRemovedEvent event) {
         Account account = event.extract();
         System.out.println(account);
     }
